d61941ef85b0c53bb6d09f6625b839eaf5336135
Dean Flood <deanflood1991@gmail.com>
2016-10-14 10:21:30 +0100

    Explained commands and params

--- week4/cntlm.sh
+++ week4/cntlm.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
 ps -ef | grep cntlm
+#ps prints out the proceeses running as user
+#ps -e prints out all running processes
+#ps -f formats it so that you can see 
+# UID PID PPID C STIME TTY TIME CMD
+
+# | Is the pipe command, which allows the output 
+#of one file to be entered as the input of the other
+
+#grep is "Global search a Regular Expression and Print
+# Allows you to find a Regex in a file or set of txt
+
 
